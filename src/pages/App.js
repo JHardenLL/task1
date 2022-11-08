acb0ab9a95f99f5e9e4d0595e3ae8dfa50fb371d
JackLL <3400407358@qq.com>
2022-11-08 23:43:26 +0800

    2删掉了一些没必要的代码

--- src/pages/App.js
+++ src/pages/App.js
@@ -10,11 +10,8 @@ export default function App() {
 
   function funbutton1() {
 
-    // var unarr=[];
-    // unarr.push(username,content);
-    // alert(unarr[1]);
     localStorage.setItem(username,content)
-    // var x=localStorage.getItem(localStorage.key(0))
+    
     
   
     document.getElementById('text1').value='';
@@ -40,13 +37,7 @@ export default function App() {
         </form>
       </div>
       
-      {/* <div className="pinglunqv">
-        <form action="">
-            <h3 className="title"><b>第一个用户{username}</b></h3>
-            <p>第一条评论{content}</p>
-            <input className="button1" type="button" value="删除"></input>
-        </form>
-      </div> */}
+      
       <div>
         <Commentast/>
       </div>
@@ -59,12 +50,7 @@ export default function App() {
 
 
 function Comment(props) {
-  // var ind
-  // for(ind in localStroage.getItem()){
-  //   "111"
-  // }
-  // var ind=props.index;
-  // alert(props.index)
+  
   return (
 
     <div className="pinglunqv">
@@ -72,8 +58,8 @@ function Comment(props) {
           <h3 className="title"><b>{props.k}</b></h3>
           
           <p>{props.val}</p>
-          {/* <input className="button1" onClick={localStorage.removeItem(localStorage.key(props.k))} type="button" value="删除"></input> */}
-          <input className="button1" type="button" value="删除"></input>
+          <input className="button1" onClick={localStorage.removeItem(localStorage.key(props.k))} type="button" value="删除"></input>
+          {/* <input className="button1" type="button" value="删除"></input> */}
       </form>
     </div>
   );
@@ -83,15 +69,12 @@ function Commentast(){
 
   //localStorage.clear();  
   var arr=[]; 
-  // var i=0;
-  // alert(localStorage.length);
+  
   for(let i=0; i < localStorage.length; i++){
     // alert(i);
     var localkey=localStorage.key(i);
     var localval=localStorage.getItem(localkey);
-    // alert(localkey);
-    // alert(localval);
-    // ReactDOM.render(<Comment key={localkey} val={localval}/>,document.getElementById('root'));
+    
     arr.push(<Comment val={localval} k={localkey} index={i}/>);
   }
   return arr;
